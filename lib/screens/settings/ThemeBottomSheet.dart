363377a1fe29f4436953305a8b3db9429f53b7c8
bendo <abdoelbendary8@gmail.com>
2024-02-14 02:31:30 +0200

    provider & theming

--- lib/screens/settings/ThemeBottomSheet.dart
+++ lib/screens/settings/ThemeBottomSheet.dart
@@ -4,16 +4,14 @@ import 'package:todo_app1/providers/app_config_provider.dart';
 import 'package:todo_app1/theme/AppTheme.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-
-class ShowLanguageBottomSheet extends StatefulWidget {
-  ShowLanguageBottomSheet();
-  
+class ShowThemBottomSheet extends StatefulWidget {
+  ShowThemBottomSheet();
 
   @override
-  State<ShowLanguageBottomSheet> createState() => _ShowLanguageBottomSheetState();
+  State<ShowThemBottomSheet> createState() => _ShowThemBottomSheetState();
 }
 
-class _ShowLanguageBottomSheetState extends State<ShowLanguageBottomSheet> {
+class _ShowThemBottomSheetState extends State<ShowThemBottomSheet> {
   @override
   Widget build(BuildContext context) {
     var provider = Provider.of<AppConfigProvider>(context);
@@ -31,23 +29,23 @@ class _ShowLanguageBottomSheetState extends State<ShowLanguageBottomSheet> {
           children: [
             GestureDetector(
               onTap: () {
-                provider.changeLanguage("en");
+                provider.changeTheme(ThemeMode.light);
               },
-              child: provider.appLanguage == "en"
-                  ? getselectedWidget(AppLocalizations.of(context)!.english)
-                  : getUnselectedWidget(AppLocalizations.of(context)!.english),
+              child: provider.appTheme == ThemeMode.light
+                  ? getselectedWidget(AppLocalizations.of(context)!.light)
+                  : getUnselectedWidget(AppLocalizations.of(context)!.light),
             ),
             Divider(
-              thickness: 2,
+              thickness: 4,
               color: AppTheme.greyColor,
             ),
             GestureDetector(
               onTap: () {
-                provider.changeLanguage("ar");
+                provider.changeTheme(ThemeMode.dark);
               },
-              child: provider.appLanguage == "ar"
-                  ? getselectedWidget(AppLocalizations.of(context)!.arabic)
-                  : getUnselectedWidget(AppLocalizations.of(context)!.arabic),
+              child: provider.isDark()
+                  ? getselectedWidget(AppLocalizations.of(context)!.dark)
+                  : getUnselectedWidget(AppLocalizations.of(context)!.dark),
             ),
           ],
         ),
@@ -68,7 +66,7 @@ class _ShowLanguageBottomSheetState extends State<ShowLanguageBottomSheet> {
         ),
         Icon(
           Icons.check_circle,
-          size: 40,
+          size: 30,
           color: AppTheme.primaryColor,
         )
       ],
@@ -76,16 +74,17 @@ class _ShowLanguageBottomSheetState extends State<ShowLanguageBottomSheet> {
   }
 
   Widget getUnselectedWidget(String text) {
-    return Container(
-      height: 50,
-      width: double.infinity,
-      child: Text(
-        text,
-        style: Theme.of(context)
-            .textTheme
-            .titleLarge!
-            .copyWith(color: AppTheme.primaryColor, fontSize: 28),
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          text,
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: AppTheme.primaryColor, fontSize: 28),
+        ),
+      ],
     );
   }
 }
--- lib/theme/AppTheme.dart
+++ lib/theme/AppTheme.dart
@@ -10,6 +10,8 @@ class AppTheme {
   static Color blackColorDark = const Color(0xff060E1E);
   static Color greyColor = const Color(0xffC8C9CB);
   static Color backgrounColorDark = const Color(0xff060E1E);
+  static Color bottomAppBarColorDark = const Color(0xff141922);
+  static Color cardColorDark = const Color(0xff141922);
   static ThemeData lightMode = ThemeData(
     primaryColor: primaryColor,
     scaffoldBackgroundColor: backgroundColor,
@@ -21,6 +23,52 @@ class AppTheme {
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(35),
             side: BorderSide(color: whiteColor, width: 4))),
+    bottomSheetTheme: const BottomSheetThemeData(
+        shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(25),
+        topRight: Radius.circular(25),
+      ),
+    )),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedIconTheme: const IconThemeData(size: 40),
+      unselectedIconTheme: const IconThemeData(size: 30),
+      selectedItemColor: primaryColor,
+      unselectedItemColor: greyColor,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+    ),
+    textTheme: TextTheme(
+      titleLarge: TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.bold,
+        color: whiteColor,
+      ),
+      titleSmall: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        color: blackColor,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 15,
+        fontWeight: FontWeight.w400,
+        color: primaryColor,
+      ),
+    ),
+  );
+  static ThemeData darkMode = ThemeData(
+    primaryColor: primaryColor,
+    scaffoldBackgroundColor: backgrounColorDark,
+    appBarTheme: AppBarTheme(backgroundColor: primaryColor),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+        iconSize: 40,
+        backgroundColor: primaryColor,
+        foregroundColor: whiteColor,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(35),
+            side: BorderSide(color: bottomAppBarColorDark, width: 4))),
     bottomSheetTheme: const BottomSheetThemeData(
         shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.only(
